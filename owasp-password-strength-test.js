03243dc3f769617954778aaab4ac015d7389bc95
lrossy <lrossy@gmail.com>
2016-07-17 01:04:23 -0400

    changed pwd requirements

--- owasp-password-strength-test.js
+++ owasp-password-strength-test.js
@@ -17,8 +17,8 @@
     // strength
     owasp.configs = {
       allowPassphrases       : true,
-      maxLength              : 128,
-      minLength              : 10,
+      maxLength              : 20,
+      minLength              : 6,
       minPhraseLength        : 20,
       minOptionalTestsToPass : 4,
     };
@@ -74,32 +74,32 @@
       optional: [
 
         // require at least one lowercase letter
-        function(password) {
-          if (!/[a-z]/.test(password)) {
-            return 'The password must contain at least one lowercase letter.';
-          }
-        },
-
-        // require at least one uppercase letter
-        function(password) {
-          if (!/[A-Z]/.test(password)) {
-            return 'The password must contain at least one uppercase letter.';
-          }
-        },
-
-        // require at least one number
-        function(password) {
-          if (!/[0-9]/.test(password)) {
-            return 'The password must contain at least one number.';
-          }
-        },
-
-        // require at least one special character
-        function(password) {
-          if (!/[^A-Za-z0-9]/.test(password)) {
-            return 'The password must contain at least one special character.';
-          }
-        },
+        // function(password) {
+        //   if (!/[a-z]/.test(password)) {
+        //     return 'The password must contain at least one lowercase letter.';
+        //   }
+        // },
+        //
+        // // require at least one uppercase letter
+        // function(password) {
+        //   if (!/[A-Z]/.test(password)) {
+        //     return 'The password must contain at least one uppercase letter.';
+        //   }
+        // },
+        //
+        // // require at least one number
+        // function(password) {
+        //   if (!/[0-9]/.test(password)) {
+        //     return 'The password must contain at least one number.';
+        //   }
+        // }
+        //
+        // // require at least one special character
+        // function(password) {
+        //   if (!/[^A-Za-z0-9]/.test(password)) {
+        //     return 'The password must contain at least one special character.';
+        //   }
+        // },
 
       ],
     };
